f4a0509aad351f56d01314a0e4781021ea8de53f
catastropi <125595274+catastropi@users.noreply.github.com>
2025-03-15 23:18:06 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,19 @@
+N, M = input().split()
+
+array = []
+
+
+for I in range(1, int(N)+1):
+  array.append(I)
+
+array2 = array.copy()
+
+for I in range(1, int(M)+1):
+  i, j= input().split()
+  array2 = array.copy()
+  for m in range(int(i), int(j)+1):
+    array[m-1] = array2[(int(i)-1) + (int(j)-1)-m+1]
+    
+
+for o in array:
+  print(o, end = " ")
