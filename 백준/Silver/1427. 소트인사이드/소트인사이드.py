1e52daadd9198dd3d21e037f033fc187805eccba
catastropi <125595274+catastropi@users.noreply.github.com>
2025-12-09 21:36:36 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,11 +1,4 @@
-N = str(input())
-
-l = []
-
-for i in range(len(N)):
-  l.append(int(N[i]))
-
-l.sort(reverse=True)
-L = ''.join(map(str, l))
-
-print(L)
+import sys
+n=sys.stdin.readline().rstrip()
+n_=sorted(n,reverse=True)
+print(''.join(n_))
